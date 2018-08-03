ccd4969775fe83e138b80f028d6ffbd2d9efe613
caimiao <caimiao@kingsoft.com>
2018-08-03 11:37:55 +0800

    handle confliction

--- models/mysql/build_tables.py
+++ models/mysql/build_tables.py
@@ -12,12 +12,20 @@ Change Activity:
 """
 __author__ = 'caimmy'
 
+import os, sys
+
+p = os.getcwd()
+root_path = os.path.dirname(os.path.dirname(p))
+sys.path.append(root_path)
 from models.mysql.db import engine, Base
 
 from models.mysql.tables import *
 from models.mysql.enterprise_tbls import *
 
 if "__main__" == __name__:
+    """
+    
     res = Base.metadata.create_all(engine)
     print(res)
     print("completed")
+    """
