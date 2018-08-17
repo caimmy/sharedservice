daac81100ed76d4d19fdc10662b8fdb85d61ed86
caimiao <caimiao@kingsoft.com>
2018-08-17 16:06:40 +0800

    add rebuild mysql tables command.

--- models/mysql/build_tables.py
+++ models/mysql/build_tables.py
@@ -13,16 +13,50 @@ Change Activity:
 __author__ = 'caimmy'
 
 import os, sys
-
+from datetime import datetime
 p = os.getcwd()
 root_path = os.path.dirname(os.path.dirname(p))
 sys.path.append(root_path)
 from models.mysql.db import engine, Base
+from sqlalchemy.orm import sessionmaker, session
 
 from models.mysql.tables import *
 from models.mysql.enterprise_tbls import *
 
+def init_Tables():
+    Session = sessionmaker(bind=engine, autocommit=False, autoflush=True, expire_on_commit=False)
+    conn = Session()
+    try:
+        enterprise = Enterprise()
+        enterprise.name = "瑞某科技"
+        enterprise.email = "ruimou@qq.com"
+        enterprise.validaion = '1'
+        enterprise.expire_tm = enterprise.create_tm = datetime.now()
+        enterprise.salt = '1'
+        enterprise.passwd = "2"
+        conn.add(enterprise)
+        conn.flush()
+
+        platuser = PlatUser()
+        platuser.name = "caimmy"
+        platuser.phone = "19981299007"
+        platuser.email = "ruimou@qq.com"
+        platuser.salt, platuser.passwd = platuser.genPassword("abcd1234")
+        platuser.create_tm = datetime.now()
+        platuser.ep = enterprise.id
+        conn.add(platuser)
+        conn.commit()
+        print("init table success")
+    except Exception as e:
+        conn.rollback()
+        print(e)
+
+
+
 if "__main__" == __name__:
+    drop_res = Base.metadata.drop_all(engine)
+    print(drop_res)
     res = Base.metadata.create_all(engine)
     print(res)
-    print("completed")
+
+    init_Tables()
